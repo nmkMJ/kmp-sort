33a6fccc3958de3bef15d3f30718266a0b16659a
nmkMJ <44439626+nmkMJ@users.noreply.github.com>
2018-12-07 01:17:31 +0800

    Add files via upload

---
+++ kmp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+void kmpMatching(char *text, char *word) {
+	int failTable[strlen(word)];
+	failTable[0] = 0;
+
+	for(int i = 1, j = 0; i < strlen(word); i++, j++) {
+		if(word[j] == word[i]) 
+			failTable[i] = failTable[i - 1] + 1;
+		else { 
+			int check = 0;
+			while(j - 1 >= 0) {
+				j = failTable[j - 1];
+				if(word[j] == word[i]) {
+					check = 1;
+					failTable[i] = j + 1;
+					break;
+				}
+			}
+			if(!check) failTable[i] = 0; 
+		}
+	}
+
+	for(int i = 0, j = 0; i < strlen(text); i++) {
+		if(j == strlen(word)) {
+			printf("%d mornoos %d mor hurtel oldov\n", i - j, i);
+			return;
+		}
+		if(text[i] == word[j])
+			j++;
+		else 
+			if(j > 0) j = failTable[j - 1];
+			else j = 0;
+	}
+}
+
+int main() {
+	kmpMatching("BC ABCDAB ABCDABCDABDE", "BC");
+}
